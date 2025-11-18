843e051f9684b4f29d1893f891f0091e4a6175c7
Stephen von Takach <steve@place.technology>
2025-11-18 23:33:09 +1100

    feat: Certification Declaration validation passes

--- src/matter/certificate/certification_declaration.cr
+++ src/matter/certificate/certification_declaration.cr
@@ -28,32 +28,52 @@ module Matter
 
       # Build the CD TLV structure
       # Based on Matter Core Spec section 6.3.1
+      # Note: Matter spec uses 0-based field tags (0-8), not 1-based
+      # Note: Matter spec requires specific integer sizes - must use force_size
       private def self.build_cd_tlv(vendor_id : UInt16, product_id : UInt16) : Bytes
         io = IO::Memory.new
         writer = TLV::Writer.new(io)
 
-        # Product ID array with proper type
-        product_id_array = [product_id.as(TLV::Value)] of TLV::Value
+        # Start structure (anonymous)
+        writer.start_structure(nil)
 
-        # Certification Declaration TLV structure
-        data = {
-          1_u8 => 1_u16,                 # formatVersion = 1
-          2_u8 => vendor_id,             # vendorId
-          3_u8 => product_id_array,      # productIdArray
-          4_u8 => 22_u32,                # deviceTypeId = 22 (Root Node)
-          5_u8 => "CSA00000SWC00000-00", # certificateId
-          6_u8 => 0_u8,                  # securityLevel = 0
-          7_u8 => 0_u16,                 # securityInformation = 0
-          8_u8 => 1_u16,                 # versionNumber = 1
-          9_u8 => 0_u8,                  # certificationType = 0 (development/test)
-        } of TLV::Tag => TLV::Value
+        # Field 0: formatVersion (uint8)
+        writer.put_unsigned_int(0_u8, 1_u8)
 
-        writer.put(nil, data)
+        # Field 1: vendorId (uint16)
+        writer.put_unsigned_int(1_u8, vendor_id, force_size: 2)
+
+        # Field 2: productIdArray (array of uint16)
+        writer.start_array(2_u8)
+        writer.put_unsigned_int(nil, product_id, force_size: 2)
+        writer.end_container
+
+        # Field 3: deviceTypeId (uint32 - MUST be 4 bytes)
+        writer.put_unsigned_int(3_u8, 22_u32, force_size: 4)
+
+        # Field 4: certificateId (string)
+        writer.put_string(4_u8, "CSA00000SWC00000-00")
+
+        # Field 5: securityLevel (uint8)
+        writer.put_unsigned_int(5_u8, 0_u8)
+
+        # Field 6: securityInformation (uint16 - MUST be 2 bytes)
+        writer.put_unsigned_int(6_u8, 0_u16, force_size: 2)
+
+        # Field 7: versionNumber (uint16 - MUST be 2 bytes)
+        writer.put_unsigned_int(7_u8, 1_u16, force_size: 2)
+
+        # Field 8: certificationType (uint8)
+        writer.put_unsigned_int(8_u8, 0_u8)
+
+        # End structure
+        writer.end_container
+
         io.rewind.to_slice
       end
 
       # Build PKCS#7 SignedData ASN.1 structure
-      # Uses the DERCodec helpers for proper encoding
+      # Manually builds DER bytes in correct RFC 5652 field order
       private def self.build_pkcs7_signed_data(content : Bytes) : Bytes
         # Load the test private key
         key_bytes = Bytes.new(TEST_CMS_SIGNER_PRIVATE_KEY.hexbytes.size)
@@ -78,30 +98,66 @@ module Matter
           ski_bytes[i] = b
         end
 
-        # Build PKCS#7 SignedData structure
-        # Based on RFC 5652 (CMS) and Matter spec requirements
-        signer_info = {
-          "version"            => 3_u8,                                                        # Signer info version 3
-          "sid"                => Codec::DERCodec::ContextTaggedSlice.new(0, ski_bytes).value, # Subject Key ID
-          "digestAlgorithm"    => Codec::DERCodec::SHA256_CMS.call,                            # SHA-256
-          "signatureAlgorithm" => Codec::DERCodec::EcdsaWithSHA256_X962.call,                  # ECDSA with SHA-256
-          "signature"          => signature,                                                   # ECDSA signature
-        } of String => Codec::DERCodec::Value
+        # Build SignerInfo SEQUENCE manually in RFC 5652 order:
+        # version, sid, digestAlgorithm, signatureAlgorithm, signature
+        signer_info_fields = [] of Bytes
+        signer_info_fields << Codec::DERCodec::Base.encode(3_u8) # version
 
-        signer_infos = [signer_info.as(Codec::DERCodec::Value)] of Codec::DERCodec::Value
+        # Build SubjectKeyIdentifier as context-specific [0] primitive (just raw bytes)
+        ski_io = IO::Memory.new
+        ski_io.write_byte(0x80_u8) # Context-specific [0] (primitive)
+        ski_length_bytes = Codec::DERCodec::Base.encode_length_bytes(ski_bytes.size)
+        ski_io.write(ski_length_bytes)
+        ski_io.write(ski_bytes)
+        signer_info_fields << ski_io.to_slice # sid
 
-        signed_data = {
-          "version"          => 3_u8,                                                         # CMS version 3
-          "digestAlgorithms" => [Codec::DERCodec::SHA256_CMS.call] of Codec::DERCodec::Value, # SHA-256
-          "encapContentInfo" => {
-            "eContentType" => Codec::DERCodec::Pkcs7Data.call(content), # Encapsulated content
-          } of String => Codec::DERCodec::Value,
-          "signerInfos" => signer_infos,
-        } of String => Codec::DERCodec::Value
+        signer_info_fields << Codec::DERCodec::Base.encode(Codec::DERCodec::SHA256_CMS.call)           # digestAlgorithm
+        signer_info_fields << Codec::DERCodec::Base.encode(Codec::DERCodec::EcdsaWithSHA256_X962.call) # signatureAlgorithm
+        signer_info_fields << Codec::DERCodec::Base.encode(signature)                                  # signature
+        signer_info_seq = Codec::DERCodec::Base.encode_sequence(Slice(UInt8).join(signer_info_fields))
 
-        # Encode as DER
-        pkcs7 = Codec::DERCodec::Pkcs7SignedData.call(signed_data)
-        Codec::DERCodec::Base.encode(pkcs7)
+        # Build SET OF SignerInfo (tag 0x31 = SET | CONSTRUCTED)
+        io = IO::Memory.new
+        io.write_byte(0x31_u8) # SET | CONSTRUCTED
+        length_bytes = Codec::DERCodec::Base.encode_length_bytes(signer_info_seq.size)
+        io.write(length_bytes)
+        io.write(signer_info_seq)
+        signer_infos_set = io.to_slice
+
+        # Build digestAlgorithms SET (tag 0x31)
+        digest_alg_encoded = Codec::DERCodec::Base.encode(Codec::DERCodec::SHA256_CMS.call)
+        io2 = IO::Memory.new
+        io2.write_byte(0x31_u8) # SET | CONSTRUCTED
+        length_bytes2 = Codec::DERCodec::Base.encode_length_bytes(digest_alg_encoded.size)
+        io2.write(length_bytes2)
+        io2.write(digest_alg_encoded)
+        digest_algs_set = io2.to_slice
+
+        # Build SignedData SEQUENCE in RFC 5652 order:
+        # version, digestAlgorithms, encapContentInfo, signerInfos
+        signed_data_fields = [] of Bytes
+        signed_data_fields << Codec::DERCodec::Base.encode(3_u8)                                     # version
+        signed_data_fields << digest_algs_set                                                        # digestAlgorithms SET
+        signed_data_fields << Codec::DERCodec::Base.encode(Codec::DERCodec::Pkcs7Data.call(content)) # encapContentInfo
+        signed_data_fields << signer_infos_set                                                       # signerInfos SET
+        signed_data_seq = Codec::DERCodec::Base.encode_sequence(Slice(UInt8).join(signed_data_fields))
+
+        # Wrap in PKCS#7 SignedData OID manually to avoid double-encoding
+        # Structure: SEQUENCE { OID(pkcs7-signedData), [0] EXPLICIT <raw SignedData bytes> }
+
+        # Build context tag [0] EXPLICIT (CONSTRUCTED) with raw SignedData bytes
+        ctx_io = IO::Memory.new
+        ctx_io.write_byte(0xA0_u8) # Context [0] | CONSTRUCTED
+        ctx_length = Codec::DERCodec::Base.encode_length_bytes(signed_data_seq.size)
+        ctx_io.write(ctx_length)
+        ctx_io.write(signed_data_seq)
+        context_tagged = ctx_io.to_slice
+
+        # Build PKCS#7 OID
+        pkcs7_oid = Codec::DERCodec::Base.encode(Codec::DERCodec::ObjectId.new("2a864886f70d010702").value)
+
+        # Combine OID + context-tagged SignedData in a SEQUENCE
+        Codec::DERCodec::Base.encode_sequence(Slice(UInt8).join([pkcs7_oid, context_tagged]))
       end
     end
   end
